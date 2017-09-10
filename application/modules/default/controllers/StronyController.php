2b30379845acef0e5d3e02f48efb06d7d2427a62
prrzeemoo <przemekstopka@gmail.com>
2017-09-10 17:18:04 +0200

    Rozdział 7

---
+++ application/modules/default/controllers/StronyController.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Przemo
+ * Date: 2017-09-10
+ * Time: 16:47
+ */
+class StronyController extends Saffron_AbstractController
+{
+
+    public function jaszczurkaAction()
+    {
+
+    }
+
+    public function zaskroniecAction()
+    {
+
+    }
+
+    public function zmijaAction()
+    {
+
+    }
+
+    public function zolwAction()
+    {
+
+    }
+}
